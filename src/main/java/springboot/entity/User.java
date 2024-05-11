bd5c479255857d746e22fa3ba37fe1bae6c2849f
PorvaniVafo <PorvaniVafo>
2024-05-11 22:24:16 +0600

    ...

--- src/main/java/springboot/entity/User.java
+++ src/main/java/springboot/entity/User.java
@@ -2,17 +2,44 @@ package springboot.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.List;
+import java.util.Collection;
 
 @Entity
 @Data
 @Table(name="users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String email;
     private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
--- src/main/java/springboot/repositories/UserRepository.java
+++ src/main/java/springboot/repositories/UserRepository.java
@@ -1,12 +1,16 @@
 package springboot.repositories;
 
-import springboot.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import springboot.entity.User;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Boolean existsByUsername(String username);
     Boolean existsByEmail(String email);
+    Optional<User> findByUsername(String username);
+
 }
